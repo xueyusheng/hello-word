282663a1cbf3590091e64a5dd3398d581b11f24b
xueyusheng <xueyusheng@zxisl.com>
2016-08-11 14:22:25 +0800

    first commit to github

---
+++ src/main/java/xueys/test/learn_maven/App.java
@@ -0,0 +1,16 @@
+package xueys.test.learn_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void test(){
+		System.out.println("sdfsdf");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
